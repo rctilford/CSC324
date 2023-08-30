5919cf1e843eeba0969791263c0aca6da7f770c8
Rhys Tilford <rhystilford@gmail.com>
2023-08-30 19:33:21 -0400

    Completed Homework 1 for CSC324

--- ch02_rhys_tilford.js
+++ ch02_rhys_tilford.js
@@ -9,23 +9,50 @@ while(line.length <= 7){
 }
 
 // FizzBuzz
-// number = 1
-
-// if(number % 3 == 0){
-
-// }
-
+number = 1;
+while(number <= 100){
+if(number % 3 == 0){
+    if(number % 5 == 0){
+        console.log("FizzBuzz");
+    } else {
+        console.log("Fizz");
+    }
+} else {
+    if(number % 5 == 0){
+        console.log("Buzz");
+    } else {
+        console.log(number);
+    }
+}
+number = number + 1;
+}
 
 // Chessboard
 
-line = 1
-
+line = 1;
+let str = "";
 while(line <= 8){
-    str = str + if(line % 2 == 0){
-                        " # # # #\n"
-                    } else {
-                        "# # # # \n"
-                    }
+    if(line % 2 == 0){
+            str = str + "# # # # \n";
+        } else {
+            str = str + " # # # #\n";
+    }
+    line = line + 1;
 }
+console.log(str);
 
+// Variable Size Chessboard
+
+size = 8;
+line = 1;
+str = "";
+while(line <= size){
+    if(line % 2 == 0){
+            str = str + "# ".repeat(size/2) + "\n";
+        } else {
+            str = str + " #".repeat(size/2) + "\n";
+    }
+    line = line + 1;
+}
+console.log(str);
 

27c46c78fd968ed7267c3a1819b75c1bb4d560de
Rhys Tilford <rhystilford@gmail.com>
2023-09-15 01:10:41 -0400

    finished ch04_rhys_tilford HW

--- HWFiles/ch04_rhys_tilford.js
+++ HWFiles/ch04_rhys_tilford.js
@@ -3,12 +3,52 @@
 
 // Sum and Range
 
-function range(start,end){
-    let Nums = Array
-    while(i <= end - start){
-        
+function range(start, end) {
+    let Nums = Array.from([]);
+    let i = start;
+    while (i <= end) {
+        Nums.push(i);
+        i = i + 1;
     }
+    return (Nums);
+};
+
+console.log(range(3, 7));
+
+function sum(range) {
+    let i = range.length - 1;
+    while (i > 0) {
+        range[i - 1] = range[i] + range[i - 1];
+        i = i - 1;
+    };
+    return (range[0]);
 }
+
+console.log(sum(range(3, 7)));
 
 
 // Reversing an Array
+
+function reverseArray(array) {
+    revArray = Array.from([]);
+    let i = 0;
+    while (i <= array.length - 1) {
+        revArray[array.length - i] = array[i];
+        i = i + 1;
+    };
+    return (revArray.slice(1))
+}
+
+console.log(reverseArray(range(2, 5)))
+
+
+function reverseArrayInPlace(array) {
+    for (let i = 0; i < array.length / 2; i++) {
+        const hold = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = hold;
+    }
+    return(array)
+}
+
+console.log(reverseArrayInPlace([2, 3, 4, 5]));
